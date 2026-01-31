90349c77d8032b1e7ae20da8ed5350f119df29eb
strbw <khoaquang0604@gmail.com>
2026-01-31 19:23:35 +0700

    variable

---
+++ ss7_variable/input.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Entet the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area  = width * height;
+document.writeln ("Area =" + area );
